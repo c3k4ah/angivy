56787accc3edbb00a062fd121981762d5f73cf24
c3k4ah <dominickp20.aps1b@gmail.com>
2023-01-07 02:57:06 +0300

    [IMP] app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,16 @@
 import 'routes/get_all_actu.route.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_router/shelf_router.dart';
+import 'package:angivy/config/db.config.dart' as db_config;
 
 void main(List<String> arguments) async {
-  //TODO: il faut encore améliorer ce code de sorte que les route s'excute ici sans appeller leur fonction
-  getAllActus();
+//TODO: il faut encore améliorer ce code de sorte que les route s'excute ici sans appeller directment leur fonction comme ça
+
+  //recupère les donnée dans le fichier .env
+  var conf = db_config.DBCONFIG();
+  //instance de Router
+  final app = Router();
+  getAllActus(app: app);
+  //lancement du serveur
+  await io.serve(app, conf.host, conf.port);
 }
--- lib/routes/get_all_actu.route.dart
+++ lib/routes/get_all_actu.route.dart
@@ -1,22 +1,13 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
-import 'package:shelf/shelf_io.dart' as io;
-import 'package:angivy/config/db.config.dart' as db_config;
+
 import '../models/actu.model.dart';
 
-void getAllActus() async {
-  //recupère les donnée dans le fichier .env
-  var conf = db_config.DBCONFIG();
-
-  //instance de Router
-  final app = Router();
-
+void getAllActus({required Router app}) async {
   //définir une route pour renvoyer le résultat de la fonction "getActusJson" sous forme de réponse HTTP
   app.get('/actus', (Request request) async {
     var usersJson = await ActuModel().getActusJson();
     return Response.ok(usersJson,
         headers: {'Content-Type': 'application/json'});
   });
-
-  await io.serve(app, 'localhost', conf.port);
 }
